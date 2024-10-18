9b453584fd5c62a60ef9251da75538c23c1e69f0
James Taylor <r92azrfn@students.codeinstitute.net>
2024-10-18 10:14:07 +0000

    added random number generator for the runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// 
-//
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -18,9 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    // creates two random numbers between 1 and 25 for the operands variable
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
